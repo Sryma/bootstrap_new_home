1d83e9f0b93e733eb79addcd2760277608b28922
Sryma <rurahgst@gmail.com>
2017-08-25 00:20:55 -0500

    first

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+      var owl = $('.owl-carousel');
+      owl.owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true
+      });
+      })
+    
+var wall = new freewall("#freewall");
+      wall.reset({
+        selector: '.brick',
+        animate: true,
+        cellW: 200,
+        cellH: 'auto',
+        onResize: function() {
+          wall.fitWidth();
+        }
+      });
+      
+      wall.container.find('.brick img').load(function() {
+        wall.fitWidth();
+      });
+
+      //call sliphover
+      $('#freewall').sliphover();
+// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+//   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+//   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+//   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+//   ga('create', 'UA-46794744-6', 'auto');
+//   ga('send', 'pageview');
+
