be78ea275a887d7a2a20cedfc952117452ed5cc2
Sryma <rurahgst@gmail.com>
2017-08-25 20:52:40 -0500

    first

--- js/main.js
+++ js/main.js
@@ -1,12 +1,38 @@
 $(document).ready(function() {
       var owl = $('.owl-carousel');
       owl.owlCarousel({
-        items: 4,
+        items: 6,
         loop: true,
-        margin: 10,
+        margin: 5,
         autoplay: true,
         autoplayTimeout: 1000,
-        autoplayHoverPause: true
+        autoplayHoverPause: true,
+        responsiveClass: true,
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  480: {
+                    items: 2,
+                    nav: false
+                  },
+                  600: {
+                    items: 3,
+                    nav: false
+                  },
+                  768: {
+                    items: 4,
+                    nav: false
+                  },
+                  1000: {
+                    items: 5,
+                    nav: false,
+                    loop: true,
+                    margin: 5
+                  }
+                  
+                }
       });
       })
     
