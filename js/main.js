b93cdae26b2a9aac82ca952797c2ad3f08c0c9da
Sryma <rurahgst@gmail.com>
2017-10-19 19:07:08 -0500

    f

--- js/main.js
+++ js/main.js
@@ -52,29 +52,20 @@ $(document).ready(function() {
       });
       })
     
-var wall = new freewall("#freewall");
-      wall.reset({
-        selector: '.brick',
-        animate: true,
-        cellW: 200,
-        cellH: 'auto',
-        onResize: function() {
-         
-          wall.fitWidth();
-        }
-      });
-      
-      wall.container.find('.brick img').load(function() {
-        wall.fitWidth();
-      });
-
-      //call sliphover
-      $('#freewall').sliphover();
-// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-//   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-//   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-//   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-//   ga('create', 'UA-46794744-6', 'auto');
-//   ga('send', 'pageview');
-
+    
+ // $(".owl-carousel").owlCarousel({
+ //        autoPlay: true,
+ //        loop: true,
+ //        margin: 50,
+ //        responsive: {
+ //            0: {
+ //                items: 1
+ //            },
+ //            600: {
+ //                items: 3
+ //            },
+ //            1000: {
+ //                items: 5
+ //            }
+ //        }
+ //  });
